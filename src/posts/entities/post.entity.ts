fce9aa5bfc5540a5547b92547edbd88987126a1a
Devansh-Walia <devanshwalia9898@gmail.com>
2025-02-06 17:34:51 +0530

    added post entity and link to users

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,18 @@
-export class Post {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { User } from 'src/users/entities/user.entity';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user: User;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+import { Post } from 'src/posts/entities/post.entity';
 
 @Entity()
 export class User {
@@ -10,4 +12,7 @@ export class User {
 
   @Column()
   provider: string; // 'google' or 'facebook'
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 }
