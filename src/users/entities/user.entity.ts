7cade74dadebb56ce30df35183520ff7e1d4e13b
Devansh-Walia <devanshwalia9898@gmail.com>
2025-02-06 17:22:59 +0530

    user entity updated to incorporate the providers

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  provider: string; // 'google' or 'facebook'
+}
